b45189273a772a5ea3f35612323bec09d8b12c98
root <windowsnxz@163.com>
2017-05-25 05:20:52 -0400

    success

--- maven/test.sh
+++ maven/test.sh
@@ -6,5 +6,6 @@ DEV=/facilities
 cd /opt
 mvn install:install-file -DgroupId=oracle.com -DartifactId=ojdbc7 -Dversion=12.1.0.1 -Dpackaging=jar -Dfile=ojdbc7-12.1.0.1.jar
 
+
 cd $DEV
 mvn tomcat7:redeploy
