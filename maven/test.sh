9d99379a58f0d33f61891652da85fb8cf97c14a0
root <windowsnxz@163.com>
2017-05-25 00:49:21 -0400

    update driver

--- maven/test.sh
+++ maven/test.sh
@@ -2,5 +2,8 @@
 
 DEV=/facilities
 
+cd /opt
+mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar -Dfile=ojdbc7-12.1.0.1.jar
+
 cd $DEV
 mvn tomcat7:redeploy
