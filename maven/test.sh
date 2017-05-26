e84274e560f3768ad9b35a908f82b7dbf599f68e
root <windowsnxz@163.com>
2017-05-26 02:58:54 -0400

    setup profile

--- maven/test.sh
+++ maven/test.sh
@@ -7,4 +7,4 @@ cd /opt
 mvn install:install-file -DgroupId=oracle.com -DartifactId=ojdbc7 -Dversion=12.1.0.1 -Dpackaging=jar -Dfile=ojdbc7-12.1.0.1.jar
 #notice the "/" and "\" in pom.xml when exec mvn deploy
 cd $DEV
-mvn tomcat7:redeploy
+mvn -P lindev_80  tomcat7:redeploy
