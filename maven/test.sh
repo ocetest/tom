5424f31b9569a9428de5da459c00a5f30cef3adf
root <windowsnxz@163.com>
2017-05-26 02:45:09 -0400

    notice slash

--- maven/test.sh
+++ maven/test.sh
@@ -5,7 +5,6 @@ DEV=/facilities
 
 cd /opt
 mvn install:install-file -DgroupId=oracle.com -DartifactId=ojdbc7 -Dversion=12.1.0.1 -Dpackaging=jar -Dfile=ojdbc7-12.1.0.1.jar
-
-
+#notice the "/" and "\" in pom.xml when exec mvn deploy
 cd $DEV
 mvn tomcat7:redeploy
